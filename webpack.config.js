257f97f361cfef1fcc6a0038d1e09e6e77d24936
Wanlin-Lu <luwanlin@126.com>
2020-12-31 21:20:09 +0800

    build with clean-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CleanWebpackPlugin = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
 	entry: {
@@ -8,7 +8,7 @@ module.exports = {
 		print: './src/print.js'
 	},
 	plugins: [
-		new CleanWebpackPlugin(['dist']),
+		new CleanWebpackPlugin(),
 		new HtmlWebpackPlugin({
 			title: 'Output Management'
 		})
