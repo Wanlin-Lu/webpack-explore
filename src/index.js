d9116052ee8569ece5f206c01efe082aacdaa969
Wanlin-Lu <luwanlin@126.com>
2020-12-31 19:55:09 +0800

    add print.js && use in index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import _ from 'lodash'
+import PMe from './print.js'
 
 function component() {
 	var element = document.createElement('div')
+	var btn = document.createElement('button')
 
 	element.innerHTML = _.join(['hello','webpack'],' ');
+
+	btn.textContent = 'Click me check the console'
+	btn.onclick = PMe
+
+	element.appendChild(btn)
 
 	return element
 }
