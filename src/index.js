516989019c2d256b8525b97eefe5eca20d086a87
Wanlin-Lu <luwanlin@126.com>
2020-12-31 19:35:46 +0800

    clean for next chapter

--- src/index.js
+++ src/index.js
@@ -1,21 +1,9 @@
 import _ from 'lodash'
-import './style.css'
-import Buty from './buty.jpg'
-import Data from './data.xml'
 
 function component() {
 	var element = document.createElement('div')
 
 	element.innerHTML = _.join(['hello','webpack'],' ');
-	element.classList.add('hello')
-
-	// add Image
-	var myButy = new Image();
-	myButy.src = Buty
-	myButy.style.cssText = "width:200px;height:300px;"
-	element.appendChild(myButy)
-
-	console.log(Data)
 
 	return element
 }
